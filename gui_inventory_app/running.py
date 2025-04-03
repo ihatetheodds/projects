d5c4211a9deb2cfff0c5d6b4d43b699e8b0bc046
root <root@DESKTOP-SR6RKDR.localdomain>
2025-04-03 09:51:41 -0500

    second update

---
+++ gui_inventory_app/running.py
@@ -0,0 +1,26 @@
+from refresher import item,App
+import customtkinter
+import sys
+import random
+
+
+
+    
+    
+
+
+app = App()
+
+
+
+
+
+app.mainloop()
+        
+
+        
+        
+
+
+
+    
